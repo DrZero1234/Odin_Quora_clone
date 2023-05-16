ee1f666144fff514d62aadebe6ecac30a101e887
M.Kristóf <drzero123456@gmail.com>
2023-05-16 19:32:16 +0200

    header sketch

---
+++ src/firebase.jsx
@@ -0,0 +1,15 @@
+import {initializeApp} from "firebase/app";
+import {getFirestore} from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCUPBl5Oqq-aY7r66FezDd_q6q0BuaHiHo",
+  authDomain: "quora-clone-e3af3.firebaseapp.com",
+  projectId: "quora-clone-e3af3",
+  storageBucket: "quora-clone-e3af3.appspot.com",
+  messagingSenderId: "521604189429",
+  appId: "1:521604189429:web:2c3706b9b84598fec349bc",
+};
+
+const app = initializeApp(firebaseConfig)
+
+export const firestore = getFirestore(app);
